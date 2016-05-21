9b84323886d08db79b9fdaf2ea228cb84b5b8f47
coincoin38 <juliengimenez38@gmail.com>
2016-05-21 13:30:01 +0200

    JSON reader

---
+++ sampleFormGenerator/stubsConstants.swift
@@ -0,0 +1,16 @@
+//
+//  stubsConstants.swift
+//  sampleFormGenerator
+//
+//  Created by julien gimenez on 21/05/2016.
+//  Copyright © 2016 julien gimenez. All rights reserved.
+//
+
+import UIKit
+
+class stubsConstants: NSObject {
+
+    static let kExtensionJson = "json"
+    static let kStubFirstForm = "firstFormStub"
+    static let kRootFirstForm = "form"
+}
