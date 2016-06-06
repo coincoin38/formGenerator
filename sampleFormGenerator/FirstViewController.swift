5f837bdf42ad3442511da92ff66c0e2b3c4d0ecc
Julien Gimenez <jgimenez@sqli.com>
2016-06-06 09:48:40 +0200

    Textfields cells management

--- sampleFormGenerator/FirstViewController.swift
+++ sampleFormGenerator/FirstViewController.swift
@@ -17,8 +17,6 @@ class FirstViewController: UIViewController,UITableViewDelegate,UITableViewDataS
     let cellSwitchIdentifier    = "SwitchTableViewCellIdentifier"
     let cellSwitchXib           = "SwitchTableViewCell"
     
-    let nextField = [0:1, 1:2, 2:3, 3:4, 4:5, 5:6]
-    
     var contentJson : Array<Form>?
     
     override func didReceiveMemoryWarning() {
@@ -38,9 +36,17 @@ class FirstViewController: UIViewController,UITableViewDelegate,UITableViewDataS
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
        
+        let cell = setTextFieldCell(tableView, cellForRowAtIndexPath: indexPath)
+        return cell
+    }
+    
+    // MARK: - Cells content
+    
+    func setTextFieldCell(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> TextFieldTableViewCell {
+        
         let cell = tableView.dequeueReusableCellWithIdentifier(cellTextFieldIdentifier, forIndexPath: indexPath) as! TextFieldTableViewCell
         cell.setDataForCell(contentJson![indexPath.row],tag:indexPath.row,textFieldDelegate:self)
-
+        cell.tag = indexPath.row
         return cell
     }
     
@@ -49,20 +55,31 @@ class FirstViewController: UIViewController,UITableViewDelegate,UITableViewDataS
     func textFieldDidEndEditing(textField: UITextField) {
         setNewContentWithTag(textField.tag, text: textField.text!)
     }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        setNextCellWithTextField(textField)
+        return false
+    }
+    
+    // MARK: - Textfields content & navigation
 
     func setNewContentWithTag(tag:Int,text:String){
         contentJson![tag].content = text
     }
     
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
-        // Consult our dictionary to find the next field
-        if let nextTag = nextField[textField.tag] {
-            if let nextResponder = textField.superview?.viewWithTag(nextTag) {
-                // Have the next field become the first responder
-                nextResponder.becomeFirstResponder()
+    func setNextCellWithTextField(textField: UITextField){
+        
+        if ((textField.superview?.superview?.isKindOfClass(TextFieldTableViewCell)) != nil){
+            
+            let currentCell: TextFieldTableViewCell = (textField.superview?.superview as? TextFieldTableViewCell)!
+            let currentIndexPath: NSIndexPath = tableView!.indexPathForCell(currentCell)!
+            
+            if currentIndexPath.row < self.contentJson!.count-1 {
+                let nextIndexPath: NSIndexPath = NSIndexPath (forRow: currentIndexPath.row+1, inSection: currentIndexPath.section)
+                let nextCell: TextFieldTableViewCell = (tableView?.cellForRowAtIndexPath(nextIndexPath) as? TextFieldTableViewCell)!
+                nextCell.contentTextField.becomeFirstResponder()
             }
         }
-        // Return false here to avoid Next/Return key doing anything
-        return false
     }
 }
